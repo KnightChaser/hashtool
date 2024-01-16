119f4f5a9265e9ae7ae1444062de5806465095af
Lee Garam <agerio100@naver.com>
2024-01-16 16:12:10 +0900

    SHA256 subcommand

---
+++ cmd/sha256.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// sha256Cmd represents the sha256 command
+var sha256Cmd = &cobra.Command{
+	Use:   "sha256",
+	Short: "Calculate SHA256 hash digest",
+	Long:  "Calculate SHA256 hash digest of the given input as an argument",
+	Run: func(cmd *cobra.Command, args []string) {
+		input, _ := cmd.Flags().GetString("input")
+		hash := CalculateSHA256(input)
+		fmt.Println(hash)
+	},
+}
+
+func CalculateSHA256(input string) string {
+	sha256hasher := sha256.New()
+	sha256hasher.Write([]byte(input))
+	hash := sha256hasher.Sum(nil)
+	return hex.EncodeToString(hash)
+}
+
+func init() {
+	sha256Cmd.Flags().StringP("input", "i", "", "Input string for SHA256 calculation")
+
+	rootCmd.AddCommand(sha256Cmd)
+}
